3738850c89d7166f9c4b7d82f56e0f8a843d8f4e
tomgoth <tomgoth@gmail.com>
2020-05-04 12:39:53 -0600

    refesh when AppState changes to active

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useCallback, useContext } from 'react'
-import { StyleSheet, SafeAreaView, ScrollView, RefreshControl } from 'react-native'
-import RMSSDChart from './RMSSDChart'
+import React, {  useEffect, useCallback, useContext } from 'react'
+import { StyleSheet, SafeAreaView, ScrollView, RefreshControl, AppState } from 'react-native'
+// import RMSSDChart from './RMSSDChart'
 import UpdateData from './UpdateData'
 import ReadinessChart from './ReadinessChart'
 import ReadinessContext from '../context/ReadinessContext'
@@ -8,8 +8,23 @@ import ReadinessContext from '../context/ReadinessContext'
 
 export default function MainView() {
 
+    useEffect(() => {
+        AppState.addEventListener('change', handleChange);  
+      
+        return () => {
+          AppState.removeEventListener('change', handleChange);  
+        }
+      }, []);
+      
+      
+      const handleChange = (newState) => {
+        if (newState === "active") {
+            getLatestReadings()
+        }
+      }
+
     const readinessContext = useContext(ReadinessContext)
-    const { isLoading, setIsLoading, getLatestReadings } = readinessContext
+    const { isLoading, getLatestReadings } = readinessContext
 
     const onRefresh = useCallback(() => {
         getLatestReadings()
