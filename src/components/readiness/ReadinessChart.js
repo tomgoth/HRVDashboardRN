3783d497322dc6dd88cf4e538f2b4387661a991f
dmaack <dominiquemaack@gmail.com>
2020-08-27 14:51:11 -0600

    changed chart button color

--- src/components/readiness/ReadinessChart.js
+++ src/components/readiness/ReadinessChart.js
@@ -95,11 +95,12 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     chartButtons: {
-        borderColor: '#20A4F3',
-        borderWidth: 1.5,
-        // backgroundColor: '#20A4F3',
+        // borderColor: '#20A4F3',
+        // borderWidth: 1,
+        //backgroundColor: '#20A4F3',
+        backgroundColor: '#344763',
         borderRadius: 10,
-        padding: 9,
+        padding: 10,
         marginTop: 15,
         marginBottom: 10,
     },
