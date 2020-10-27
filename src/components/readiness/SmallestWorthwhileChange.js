2b1245c9904eb548f3103d5fb635d58126ae2e59
tomgoth <tomgoth@gmail.com>
2020-10-27 15:36:58 -0600

    basic swc view created

--- src/components/readiness/SmallestWorthwhileChange.js
+++ src/components/readiness/SmallestWorthwhileChange.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useContext } from 'react'
 import ReadinessContext from '../../context/readiness/ReadinessContext'
-import { Text } from 'react-native'
+import { Text, View } from 'react-native'
 
 function SmallestWorthwhileChange () {
 
@@ -12,9 +12,16 @@ function SmallestWorthwhileChange () {
     }, [])
 
     return (
-        <Text>
-            {JSON.stringify(smallestWorthwhileChange)}
-        </Text>
+        <View>
+        {Object.entries(smallestWorthwhileChange).map((entry) => {
+            return (
+            <>
+                <Text>{`${entry[0]}:`}</Text> 
+                <Text>{`${entry[1]}`}</Text>
+            </>
+                )
+        })}
+        </View>
     )
 }
 
